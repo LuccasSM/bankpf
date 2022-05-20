ef2fe5e5ce334c0e110870bb0591214a1ea6cb0f
Luccas <mathaluc2016@outlook.com>
2022-05-20 10:47:11 -0300

    ajustando itens da pasta transferencias

--- Bankpf/Controller/Pré-Login/Transferencias/Transferencias.swift
+++ Bankpf/Controller/Pré-Login/Transferencias/Transferencias.swift
@@ -29,12 +29,19 @@ class Transferencias: UIViewController {
         self.navigationItem.leftBarButtonItem = barButtonRight
         buttonRight.addTarget(self, action: #selector(returnButton), for: .touchUpInside)
         
+        self.view.addSubview(views)
+        
         NSLayoutConstraint.activate([
             buttonLeft.widthAnchor.constraint(equalToConstant: 135),
             buttonLeft.heightAnchor.constraint(equalToConstant: 50),
             
             buttonRight.widthAnchor.constraint(equalToConstant: 17),
             buttonRight.heightAnchor.constraint(equalToConstant: 17),
+            
+            views.widthAnchor.constraint(equalToConstant: 100),
+            views.heightAnchor.constraint(equalToConstant: 100),
+            views.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            views.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
         ])
     }
     
@@ -52,6 +59,16 @@ class Transferencias: UIViewController {
         button.adjustsImageWhenHighlighted = false
         button.setImage(UIImage(named: "close-white"), for: UIControl.State.normal)
         return button
+    }()
+    
+    private lazy var views: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 5
+        view.layer.borderWidth = 1
+        view.layer.borderColor = CGColor(red: 55/255, green: 73/255, blue: 154/255, alpha: 1.0)
+        return view
     }()
     
     // MARK: Navegacoes da tela
